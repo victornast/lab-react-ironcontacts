4a891b4a147a2dd1047c459065b0fe3456955f22
Victor Nastasa <victornast@gmail.com>
2021-03-02 20:49:28 +0100

    change iteration 1 to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,58 @@
+import React from 'react';
 import contacts from './contacts.json';
 import './App.css';
 
 function App() {
-  const initialFive = contacts.slice(0, 5);
-  console.log(initialFive);
+  class IronContacts extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        selectedContacts: contacts.slice(0, 5)
+      };
+    }
+
+    render() {
+      return (
+        <section>
+          <button>Add Random Contact</button>
+          <table>
+            <thead>
+              <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.selectedContacts.map((contact) => {
+                return (
+                  <tr style={{ textAlign: 'left' }} key={contact.id}>
+                    <td>
+                      <img
+                        width="50px"
+                        src={contact.pictureUrl}
+                        alt={contact.name}
+                      />
+                    </td>
+                    <td>{contact.name}</td>
+                    <td>{contact.popularity.toFixed(2)}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </section>
+      );
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>IronContacts</h1>
       </header>
       <section>
-        <table>
-          <thead>
-            <tr>
-              <th>Picture</th>
-              <th>Name</th>
-              <th>Popularity</th>
-            </tr>
-          </thead>
-          <tbody>
-            {initialFive.map((contact) => {
-              return (
-                <tr style={{ textAlign: 'left' }} key={contact.id}>
-                  <td>
-                    <img
-                      width="50px"
-                      src={contact.pictureUrl}
-                      alt={contact.name}
-                    />
-                  </td>
-                  <td>{contact.name}</td>
-                  <td>{contact.popularity.toFixed(2)}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <IronContacts />
       </section>
     </div>
   );
